fd6dbb0e0389faccae8d6767644da3e945d88e6d
Harry Merritt <harry@merritt.es>
2020-01-08 02:00:35 +0000

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+function App()
+{
+    return (
+        <Header />
+    );
 }
 
 export default App;
