3dc304c08238957dfa0f4654cc63d29180d7a7b4
Harry Merritt <harry@merritt.es>
2020-01-08 03:21:10 +0000

    Created ImageCard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
-import Header from "./components/Header/Header";
+import Header    from "./components/Header/Header";
+import ImageCard from "./components/ImageCard/ImageCard";
 import "./App.css";
 
 function App()
 {
     return (
-        <Header />
+        <>
+            <Header />
+            <div className="container">
+                <div className="imageCards-container">
+                    <ImageCard src="https://apod.nasa.gov/apod/image/2001/IC405hp_ColesHelm_3447.jpg" />
+                </div>
+            </div>
+        </>
     );
 }
 
